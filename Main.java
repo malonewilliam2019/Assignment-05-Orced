189125c7b0713b9f7fc43766c70a3d4aa4ac5305
malonewilliam2019 <42677942+malonewilliam2019@users.noreply.github.com>
2018-10-07 12:23:10 -0700

    Add files via upload
    Used random number generator to calculate damage multiplier

---
+++ Main.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand= new Random();
+
+        int Int=20;
+        int dam=Int*5;
+
+        double mult1=rand.nextDouble()+.5;
+        double DAM1=dam*mult1;
+
+        double mult2=rand.nextDouble()+.5;
+        double DAM2=dam*mult2;
+
+        double mult3=rand.nextDouble()+.5;
+        double DAM3=dam*mult3;
+
+        double mult4=rand.nextDouble()+.5;
+        double DAM4=dam*mult4;
+
+        double mult5=rand.nextDouble()+.5;
+        double DAM5=dam*mult5;
+
+        double mult6=rand.nextDouble()+.5;
+        double DAM6=dam*mult6;
+
+        double mult7=rand.nextDouble()+.5;
+        double DAM7=dam*mult7;
+
+        double mult8=rand.nextDouble()+.5;
+        double DAM8=dam*mult8;
+
+        double mult9=rand.nextDouble()+.5;
+        double DAM9=dam*mult9;
+
+        double mult10=rand.nextDouble()+.5;
+        double DAM10=dam*mult10;
+
+        int tot=(int)(DAM1+DAM2+DAM3+DAM4+DAM5+DAM6+DAM7+DAM8+DAM9+DAM10);
+        System.out.println(tot);
+
+        int Int1=10;
+        double dam1=Int1*10.5;
+
+        double mult11=rand.nextDouble()+.5;
+        double DAM11=dam1*mult11;
+
+        double mult12=rand.nextDouble()+.5;
+        double DAM12=dam1*mult12;
+
+        double mult13=rand.nextDouble()+.5;
+        double DAM13=dam1*mult13;
+
+        double mult14=rand.nextDouble()+.5;
+        double DAM14=dam1*mult14;
+
+        double mult15=rand.nextDouble()+.5;
+        double DAM15=dam1*mult15;
+
+        double mult16=rand.nextDouble()+.5;
+        double DAM16=dam1*mult16;
+
+        double mult17=rand.nextDouble()+.5;
+        double DAM17=dam1*mult17;
+
+        double mult18=rand.nextDouble()+.5;
+        double DAM18=dam1*mult18;
+
+        double mult19=rand.nextDouble()+.5;
+        double DAM19=dam1*mult19;
+
+        double mult20=rand.nextDouble()+.5;
+        double DAM20=dam1*mult20;
+
+        int tot1=(int)(DAM11+DAM12+DAM13+DAM14+DAM15+DAM16+DAM17+DAM18+DAM19+DAM20);
+
+        System.out.println(tot1);
+    }
+}
